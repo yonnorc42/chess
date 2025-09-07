e55b181bb567b4c32006ec63be6ab1951431fe22
Connor Young <yonnorc42@gmai.com>
2025-09-06 23:59:08 -0600

    p(0) RookMovesCalculator implemented, passing all rook tests

--- shared/src/main/java/chess/RookMovesCalculator.java
+++ shared/src/main/java/chess/RookMovesCalculator.java
@@ -8,6 +8,82 @@ public class RookMovesCalculator implements PieceMovesCalculator {
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
         Collection<ChessMove> validMoves = new ArrayList<>();
 
+        ChessGame.TeamColor myColor = board.getPiece(myPosition).getTeamColor();
+
+        // Moves above rook
+        for (int col= myPosition.getColumn() + 1; col<=8; col++) {
+            ChessPosition possiblePosition = new ChessPosition(myPosition.getRow(), col);
+            ChessMove possibleMove = new ChessMove(myPosition, possiblePosition, null);
+            // Spot is empty
+            if (board.getPiece(possiblePosition) == null) {
+                validMoves.add(possibleMove);
+            }
+            // Spot has teams color, can't move there or past, break loop
+            else if (board.getPiece(possiblePosition).getTeamColor() == myColor) {
+                break;
+            }
+            // Spot has other teams color, can move there but not past, add move and break loop
+            else {
+                validMoves.add(possibleMove);
+                break;
+            }
+        }
+
+        // Moves below rook
+        for (int col= myPosition.getColumn() - 1; col>=1; col--) {
+            ChessPosition possiblePosition = new ChessPosition(myPosition.getRow(), col);
+            ChessMove possibleMove = new ChessMove(myPosition, possiblePosition, null);
+            // Spot is empty
+            if (board.getPiece(possiblePosition) == null) {
+                validMoves.add(possibleMove);
+            }
+            // Spot has teams color, can't move there or past, break loop
+            else if (board.getPiece(possiblePosition).getTeamColor() == myColor) {
+                break;
+            }
+            // Spot has other teams color, can move there but not past, add move and break loop
+            else {
+                validMoves.add(possibleMove);
+                break;
+            }
+        }
+
+        // Moves to right of rook
+        for (int row= myPosition.getRow() + 1; row<=8; row++) {
+            ChessPosition possiblePosition = new ChessPosition(row, myPosition.getColumn());
+            ChessMove possibleMove = new ChessMove(myPosition, possiblePosition, null);
+            // Spot is empty
+            if (board.getPiece(possiblePosition) == null) {
+                validMoves.add(possibleMove);
+            }
+            // Spot has teams color, can't move there or past, break loop
+            else if (board.getPiece(possiblePosition).getTeamColor() == myColor) {
+                break;
+            }
+            // Spot has other teams color, can move there but not past, add move and break loop
+            else {
+                validMoves.add(possibleMove);
+                break;
+            }
+        }
+        // Moves to left of rook
+        for (int row= myPosition.getRow() - 1; row>=1; row--) {
+            ChessPosition possiblePosition = new ChessPosition(row, myPosition.getColumn());
+            ChessMove possibleMove = new ChessMove(myPosition, possiblePosition, null);
+            // Spot is empty
+            if (board.getPiece(possiblePosition) == null) {
+                validMoves.add(possibleMove);
+            }
+            // Spot has teams color, can't move there or past, break loop
+            else if (board.getPiece(possiblePosition).getTeamColor() == myColor) {
+                break;
+            }
+            // Spot has other teams color, can move there but not past, add move and break loop
+            else {
+                validMoves.add(possibleMove);
+                break;
+            }
+        }
         return validMoves;
     }
 }
