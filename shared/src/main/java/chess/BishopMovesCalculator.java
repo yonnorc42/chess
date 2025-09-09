a40be48737e1f57c8e6bcacc00ccc1f382609166
Connor Young <yonnorc42@gmai.com>
2025-09-09 15:14:52 -0600

    p(0) Code quality check fixes, got rid of duplicate blocks in Bishop, Queen, and King calculators

--- shared/src/main/java/chess/BishopMovesCalculator.java
+++ shared/src/main/java/chess/BishopMovesCalculator.java
@@ -3,12 +3,12 @@ package chess;
 import java.util.ArrayList;
 import java.util.Collection;
 
+import static chess.MoveCalculatorUtils.iterateDirections;
+
 public class BishopMovesCalculator implements PieceMovesCalculator {
     @Override
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
         Collection<ChessMove> validMoves = new ArrayList<>();
-
-        ChessGame.TeamColor myColor = board.getPiece(myPosition).getTeamColor();
 
         int[][] directions = {
                 {-1,  1},
@@ -17,36 +17,8 @@ public class BishopMovesCalculator implements PieceMovesCalculator {
                 { 1, -1}
         };
 
-        for (int[] dir : directions) {
-            int row = myPosition.getRow();
-            int col = myPosition.getColumn();
+        iterateDirections(board, myPosition, directions, validMoves);
 
-            while (true) {
-                row += dir[0];
-                col += dir[1];
-
-                // Stop if off board
-                if (row < 1 || row > 8 || col < 1 || col > 8) {
-                    break;
-                }
-                ChessPosition possiblePosition = new ChessPosition(row, col);
-                ChessMove possibleMove = new ChessMove(myPosition, possiblePosition, null);
-                // Spot is empty
-                if (board.getPiece(possiblePosition) == null) {
-                    validMoves.add(possibleMove);
-                }
-                // Spot has teams color, can't move there or past, break loop
-                else if (board.getPiece(possiblePosition).getTeamColor() == myColor) {
-                    break;
-                }
-                // Spot has other teams color, can move there but not past, add move and break loop
-                else {
-                    validMoves.add(possibleMove);
-                    break;
-                }
-            }
-
-        }
         return validMoves;
     }
 }
